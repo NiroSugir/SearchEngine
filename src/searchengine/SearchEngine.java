47e71e3e1c93a0dda5c2e41ed27f9d77aed9b4b3
Niroshan Sugirtharatnam <nirosugir@gmail.com>
2019-11-30 14:12:17 -0500

    Init

---
+++ src/searchengine/SearchEngine.java
@@ -0,0 +1,16 @@
+package searchengine;
+
+/**
+ *
+ * @author Niro
+ */
+public class SearchEngine {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
