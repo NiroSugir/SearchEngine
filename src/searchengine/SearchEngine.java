0a477117c52a9a87e4f2617e476d1b00f92d922a
Niroshan Sugirtharatnam <nirosugir@gmail.com>
2019-11-30 15:46:03 -0500

    Create Website objects from Strings

--- src/searchengine/SearchEngine.java
+++ src/searchengine/SearchEngine.java
@@ -10,7 +10,25 @@ public class SearchEngine {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        String[] html = {
+            "In this course we will learn the Java programming language",
+            "In this program we learned Java and other programming languages"
+        };
+
+        // create a website object out of html text
+        Website[] websites = new Website[html.length];
+        for (int i = 0; i < html.length; i++) {
+            websites[i] = new Website(i + 1, html[i]);
+
+        }
+
+        // add website objects to an index table
+        //   - sets parsed to false
+        // index table retrieves keywords and positions of each word and adds
+        //      that info as columns in the table
+        //   - sets parsed to true for website object
+        // target: a searchable table of keywords that contains the page the
+        // word appears on as well as the position on the page
     }
-    
+
 }
---
+++ src/searchengine/Website.java
@@ -0,0 +1,20 @@
+package searchengine;
+
+/**
+ *
+ * @author Niro
+ */
+public class Website {
+
+    private int id;
+    private String content;
+
+    Website(int id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public String[] getKeywords() {
+        return this.content.split(" ");
+    }
+}
